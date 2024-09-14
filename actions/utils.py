6a8a6c6ada34a73ebbe76ec8a1a9861cabb9279b
Muhammadzakhid <m92817516@gmail.com>
2024-09-14 20:02:37 +0500

    Action file changed

--- actions/utils.py
+++ actions/utils.py
@@ -5,10 +5,11 @@ from django.utils import timezone
 from .models import Action
 
 
-def created_action(user, verb, terget=None):
+def create_action(user, verb, target=None):
     now = timezone.now()
     last_minute = now - datetime.timedelta(seconds=60)
     similar_actions = Action.objects.filter(
         user_id=user.id,
-        
+        verb=verb,
+        created__gte=last_minute
     )
